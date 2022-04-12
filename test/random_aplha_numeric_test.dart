f29959ac51123f0ac51f95917b66551697b2ac6c
Guillaume Roux <rouxguillaume8@gmail.com>
2022-04-12 16:30:34 +0200

    refacto: added linting rules from lint package

--- test/random_aplha_numeric_test.dart
+++ test/random_aplha_numeric_test.dart
@@ -1,18 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:matomo/random_alpha_numeric.dart';
+import 'package:matomo/src/random_alpha_numeric.dart';
 
 void main() {
   test('test randomAlphaNumeric returns string with correct length', () {
-    final length = 64;
+    const length = 64;
     final randomString = randomAlphaNumeric(length);
     expect(randomString.length, length);
   });
 
   test('test randomAlphaNumeric returns string with allowed characters', () {
-    final length = 42;
+    const length = 42;
     final randomString = randomAlphaNumeric(length);
 
-    final RegExp exp = RegExp(r"[0-9a-zA-z]{"+length.toString()+"}");
+    final exp = RegExp("[0-9a-zA-z]{$length}");
     expect(true, exp.hasMatch(randomString));
   });
 }
