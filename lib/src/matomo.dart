7b9ad2189ed355aa9652612fcd9da4e6a9c833c7
Marvin M <39344769+M123-dev@users.noreply.github.com>
2022-05-20 18:40:36 +0200

    feat: Allow tracking of outlinks

--- lib/src/matomo.dart
+++ lib/src/matomo.dart
@@ -347,6 +347,17 @@ class MatomoTracker {
     );
   }
 
+  void trackOutlink(
+    String? link,
+  ) {
+    return _track(
+      MatomoEvent(
+        tracker: this,
+        link: link,
+      ),
+    );
+  }
+
   void _track(MatomoEvent event) {
     _queue.add(event);
   }
