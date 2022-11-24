c9e1e446a9fce82e3ecb55ae90bc18e3c5ee5a46
Pierre Monier <pierremonier@pierres-macbook-pro.home>
2022-11-24 20:30:07 +0100

    feat: add visitor test

--- test/mock/data.dart
+++ test/mock/data.dart
@@ -1,3 +1,7 @@
 final sessionLastVisiste = DateTime(2022, 1, 2, 1, 1, 1);
 final sessionFirstVisiste = DateTime(2022, 1, 1, 1, 1, 1);
 const sessionVisitCount = 1;
+const visitorId = 'visitorId';
+const forceId = '1234567890123456'; // 16 characters
+const wrongForceId = '1234';
+const userId = 'userId';
