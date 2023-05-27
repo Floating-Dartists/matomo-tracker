2fa38b53ce992494617550e658afe5dd23c58f08
Eric Prokop <eric.prokop@epnw.eu>
2023-05-27 17:08:07 +0200

    test dispatch settings using the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,12 +9,21 @@ const _matomoEndpoint = 'http://localhost:8765/matomo.php';
 const _sideId = 1;
 const _testUserId = 'Nelson Pandela';
 
+// Usage: run the example, cause some actions by clicking around,
+// close the example within 5min to prevent the dispatchment of
+// the actions, run the example again, wait at least 5min, then
+// finally check the Matomo dashboard to see if all actions are
+// there.
+const DispatchSettings dispatchSettingsEndToEndTest =
+    DispatchSettings.persistent(dequeueInterval: Duration(minutes: 5));
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MatomoTracker.instance.initialize(
     siteId: _sideId,
     url: _matomoEndpoint,
     verbosityLevel: Level.all,
+    dispatchSettings: dispatchSettingsEndToEndTest,
   );
   MatomoTracker.instance.setVisitorUserId(_testUserId);
   runApp(const MyApp());
