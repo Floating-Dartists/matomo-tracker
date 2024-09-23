2dfb72304bd3995b16dd120a43b89a3d671e40c7
Guillaume Roux <groux@veson.com>
2024-09-23 16:09:31 +0200

    feat: base implementation for MatomoGlobalObserver

---
+++ lib/src/observers/matomo_local_observer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+
+/// {@macro matomo_local_observer}
+@Deprecated(
+  'Use matomoLocalObserver instead. '
+  'This feature was deprecated in v6.0.0 and will be removed in the next major version.',
+)
+final matomoObserver = matomoLocalObserver;
+
+/// {@template matomo_local_observer}
+/// A [RouteObserver] that will track navigation events in Matomo from a widget
+/// that uses `TraceableWidgetMixin`.
+/// {@endtemplate}
+///
+/// ### Example
+///
+/// ```dart
+/// MaterialApp(
+///   navigatorObservers: [
+///     matomoLocalObserver,
+///   ],
+/// );
+final matomoLocalObserver = RouteObserver<ModalRoute<void>>();
