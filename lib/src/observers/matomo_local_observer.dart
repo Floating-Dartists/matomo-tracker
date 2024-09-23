b64a38ae7c9a4de9f271cec28c5046f13deb0d2a
Guillaume Roux <groux@veson.com>
2024-09-23 16:20:58 +0200

    chore: update doc

--- lib/src/observers/matomo_local_observer.dart
+++ lib/src/observers/matomo_local_observer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 /// {@macro matomo_local_observer}
 @Deprecated(
   'Use matomoLocalObserver instead. '
-  'This feature was deprecated in v6.0.0 and will be removed in the next major version.',
+  'This feature was deprecated in v6.0.0-dev.1 and will be removed in the next major version.',
 )
 final matomoObserver = matomoLocalObserver;
 
