67e43868a76c90c994d247fd21183a210539dad3
Steffen Haak <haak@aucentiq.com>
2025-03-20 13:39:08 +0100

    fix: for flutter 3.29 conditional import has to be dart.library.js_interop instead of dart.library.html

--- lib/src/platform_info/platform_info.dart
+++ lib/src/platform_info/platform_info.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:matomo_tracker/src/platform_info/platform_info_interface.dart';
 import 'package:matomo_tracker/src/platform_info/platform_info_io.dart'
-    if (dart.library.html) 'package:matomo_tracker/src/platform_info/platform_info_web.dart';
+    if (dart.library.js_interop) 'package:matomo_tracker/src/platform_info/platform_info_web.dart';
 
 class PlatformInfo extends PlatformInfoInterface {
   PlatformInfo._internal();
